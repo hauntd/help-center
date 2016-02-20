3b044570d54e8a71d0917cb5aa79a21b35d71bab
Alexander Kononenko <contact@hauntd.me>
2016-02-20 18:54:43 +0300

    Viewport params updated

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -14,7 +14,7 @@ AppAsset::register($this);
 <html lang="<?= Yii::$app->language ?>">
 <head>
     <meta charset="<?= Yii::$app->charset ?>">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
     <link href="//maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css" rel="stylesheet">
     <link href='//fonts.googleapis.com/css?family=Roboto:400,300,500,300italic&subset=latin,cyrillic' rel='stylesheet'
           type='text/css'>
